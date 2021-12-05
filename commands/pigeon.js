13f479469233f98baf3c09c18364567e13e8a8cb
jackwang316 <52519516+jackwang316@users.noreply.github.com>
2021-12-05 10:19:19 -0800

    Added delay functionality to pigeon
    User can specify timers before their messages are to be sent.

--- commands/pigeon.js
+++ commands/pigeon.js
@@ -1,8 +1,15 @@
 const {sendSMS} = require("../twilio-api");
+const millisecondPerSecond = 1000
 module.exports = {
     name: "pigeon",
     description: "Send messages with pigeon",
     execute(message, args){
-        return sendSMS(message, args)
+        let timer = args[args.length - 1].trim();
+        if(timer > 0) {
+            message.reply({
+                content: "Sending SMS in " + timer + " seconds"
+            })
+        }
+        return setTimeout(() => {sendSMS(message, args)}, timer * millisecondPerSecond)
     }
 }
