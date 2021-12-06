76226b058ee2e6f31e983efc46c3a417ffecf865
jackwang316 <52519516+jackwang316@users.noreply.github.com>
2021-12-05 18:28:47 -0800

    commandHandler will be called instead of sendSMS
    After specified timeout has passed, commandHandler will be called

--- commands/pigeon.js
+++ commands/pigeon.js
@@ -1,11 +1,11 @@
-const {sendSMS} = require("../twilio-api");
+const {commandHandler} = require("../handler");
 const millisecondPerSecond = 1000
 module.exports = {
     name: "pigeon",
     description: "Send messages with pigeon",
     execute(message, args){
         let timer = args[args.length - 1].trim();
-        if(timer > 0) {message.reply("Sending SMS in" + timer + " seconds")}
-        return setTimeout(() => {sendSMS(message, args)}, timer * millisecondPerSecond)
+        if(timer > 0) {message.reply("Sending message in " + timer + " seconds")}
+        return setTimeout(() => {commandHandler(message, args)}, timer * millisecondPerSecond)
     }
 }
